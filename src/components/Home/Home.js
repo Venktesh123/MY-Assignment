7613722ac471fe88372c5dff89c9705eafbb5116
Venktesh123 <8303531530venktesh@gmail.com>
2023-11-20 13:56:26 +0530

    authentication-added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,22 +1,10 @@
 import React from 'react'
-import {Link} from "react-router-dom"
-
+import Login  from '../Login/Login'
 function Home(props) {
   return (
     <div>
-        Helllo
-      <div>
-        <h1>
-        <Link to="/login">Login</Link>
-        </h1>
-        <br/>
-        <h1>
-            <Link to="/signup">SignUp</Link>
-        </h1>
-      </div>
-      <br/>
-      <br/>
-      <br/>
+        <Login/>
+      
       <h2>{props.name ?`welcome -${props.name}`: "Login Please"}</h2>
     </div>
   )
